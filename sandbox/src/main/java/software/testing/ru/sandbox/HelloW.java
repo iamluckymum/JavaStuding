979421cde803b4757d87df52007f4c0eb8829e68
iamluckymum <iamluckymum@gmail.com>
2019-02-21 08:06:15 +0200

    Created Rectangle and Square classes

--- sandbox/src/main/java/software/testing/ru/sandbox/HelloW.java
+++ sandbox/src/main/java/software/testing/ru/sandbox/HelloW.java
@@ -3,8 +3,21 @@ package software.testing.ru.sandbox;
 public class HelloW {
 
     public static void main (String [] args){
-        System.out.println ("Hello, world!");
+        hello("world");
+        hello("user");
+        hello("Elena");
+
+        Square s = new Square(5);
+        System.out.println("The area of a square with a side" + s.l + " = " + s.area());
+
+        Rectangle r = new Rectangle(4,6);
+        System.out.println("The area of a rectangle with sides" + r.a + r.b + " = " + r.area());
+
     }
 
+    public static void hello(String somebody){
+        System.out.println ("Hello, " + somebody + "!");
+
+    }
 }
 
---
+++ sandbox/src/main/java/software/testing/ru/sandbox/Square.java
@@ -0,0 +1,14 @@
+package software.testing.ru.sandbox;
+
+public class Square {
+
+    public double l;
+
+    public Square(double l){
+        this.l = l;
+    }
+
+    public double area(){
+        return this.l * this.l;
+    }
+}
